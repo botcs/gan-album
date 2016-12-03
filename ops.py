0ee9aa30b05b00304fda1f5e5f978a9e21db7df5
botcs <botos.csaba@hotmail.hu>
2016-12-03 17:47:38 +0100

    DCGAN first model

--- ops.py
+++ ops.py
@@ -21,25 +21,22 @@ def _bias_variable(size, name='b'):
     return tf.get_variable(name=name, initializer=initial)
     #return tf.Variable(initial)
 
-def linear(input, out_dim, scope_name='linear', ret_var=False):
-    with tf.variable_scope(scope_name) as scope:
+def linear(input, out_dim, name='linear', ret_var=False):
+    with tf.variable_scope(name) as scope:
         in_dim = input.get_shape().as_list()[-1]
         mat_shape = in_dim, out_dim
         W = _weight_variable(mat_shape)
 
         b = _bias_variable(out_dim)
-        out = tf.matmul(input, W) + b
+        out = tf.add(x=tf.matmul(input, W), y=b, name=name)
     
     if ret_var: return out, W, b
     
     return out
 
-def conv(input, kernel_size, depth, pad, scope_name='convolution', ret_var=False):
-    with tf.variable_scope(scope_name) as scope:
-        padding = [[0, 0], [pad, pad], [pad, pad], [0, 0]]
-        padded_input = tf.pad(input, padding, "CONSTANT")
-
-        in_D = padded_input.get_shape().as_list()[-1]
+def conv(input, kernel_size, depth, name='convolution', ret_var=False):
+    with tf.variable_scope(name) as scope:
+        in_D = input.get_shape().as_list()[-1]
 
         K = kernel_size
         D = depth
@@ -48,16 +45,16 @@ def conv(input, kernel_size, depth, pad, scope_name='convolution', ret_var=False
         bias = _bias_variable(D)
 
         lin = tf.nn.conv2d(
-            padded_input, kernel, strides=[1, 1, 1, 1], padding='SAME')
-        out = lin+bias
+            input, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        out = tf.add(x=lin, y=bias, name=name)
 
     if ret_var: return out, K, bias
     return out
     
 def conv_T(input, kernel_size, depth, 
-           stride=2, scope_name='transposed_convolution', 
+           stride=2, name='transposed_convolution', 
            ret_var=False):
-    with tf.variable_scope(scope_name) as scope:
+    with tf.variable_scope(name) as scope:
         batch, W_in, H_in, D_in = input.get_shape().as_list()
         W_out = W_in * stride
         H_out = H_in * stride
@@ -67,7 +64,7 @@ def conv_T(input, kernel_size, depth,
         # conv2d_transposed requires this workaround for static batch size
         batch = tf.shape(input)[0]
         output_shape = [batch, H_out, W_out, D_out]
-        kernel = _weight_variable([K, K, D_out, D_in], dev=0.01, name='K')
+        kernel = _weight_variable([K, K, D_out, D_in], dev=0.02, name='K')
         bias = _bias_variable(D_out)
         lin = tf.nn.conv2d_transpose(
             value=input, 
@@ -82,4 +79,4 @@ def conv_T(input, kernel_size, depth,
     return out
 
 def lrelu(x, leak=0.2, name="lrelu"):
-    return tf.maximum(x, leak*x)
+    return tf.maximum(x, leak*x, name=name)
